afd5cae47e95f2d7bd55811e1d932ab933f36407
Allen Mordukhaev <allenmor93@gmail.com>
2022-09-20 14:15:11 -0400

    commit tree e6fc37ee556f6a6cb7e5cb5de13b692f82d259e3

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,16 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validate :genres?
+
+
+    GENRE_PATTERNS = [
+        /Fiction/i,
+        /Non-Fiction/i
+    ]
+
+    def genres?
+        if GENRE_PATTERNS.none? { |genre| genre.match category}
+            errors.add(:genre, 'must be Fiction or Non Fiction')
+        end
+    end
 end
